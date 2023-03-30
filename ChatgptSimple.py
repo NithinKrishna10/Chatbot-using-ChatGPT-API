f048981a71c846167dc0ff3b297e3566ef878a7b
NithinKrishna10 <114385325+NithinKrishna10@users.noreply.github.com>
2023-03-30 16:27:03 +0530

    Update ChatgptSimple.py

--- ChatgptSimple.py
+++ ChatgptSimple.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = "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"
+openai.api_key = "paste your api key"
 
 completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "what is data sturctures "}])
 print(completion.choices[0].message.content)
